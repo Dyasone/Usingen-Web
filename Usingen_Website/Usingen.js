4863169f2501c1d192dfb6829d2b6de30efdc957
Dyasone <m.marhenke@gmx.de>
2019-12-03 15:58:43 +0100

    add all read more jsstuff

--- Usingen_Website/Usingen.js
+++ Usingen_Website/Usingen.js
@@ -12,4 +12,49 @@ function myFunction() {
       btnText.innerHTML = "Read less";
       moreText.style.display = "inline";
     }
-  }
+  };
+  function myFunction1() {
+    var dots1 = document.getElementById("dots1");
+    var more1Text = document.getElementById("more1");
+    var btn1Text = document.getElementById("myBtn1");
+  
+    if (dots1.style.display === "none") {
+      dots1.style.display = "inline";
+      btn1Text.innerHTML = "Read more...";
+      more1Text.style.display = "none";
+    } else {
+      dots1.style.display = "none";
+      btn1Text.innerHTML = "Read less";
+      more1Text.style.display = "inline";
+    }
+  };
+  function myFunction2() {
+    var dots2 = document.getElementById("dots2");
+    var more2Text = document.getElementById("more2");
+    var btn2Text = document.getElementById("myBtn2");
+  
+    if (dots2.style.display === "none") {
+      dots2.style.display = "inline";
+      btn2Text.innerHTML = "Read more...";
+      more2Text.style.display = "none";
+    } else {
+      dots2.style.display = "none";
+      btn2Text.innerHTML = "Read less";
+      more2Text.style.display = "inline";
+    }
+  };
+  function myFunction3() {
+    var dots3 = document.getElementById("dots3");
+    var more3Text = document.getElementById("more3");
+    var btn3Text = document.getElementById("myBtn3");
+  
+    if (dots3.style.display === "none") {
+      dots3.style.display = "inline";
+      btn3Text.innerHTML = "Read more...";
+      more3Text.style.display = "none";
+    } else {
+      dots3.style.display = "none";
+      btn3Text.innerHTML = "Read less";
+      more3Text.style.display = "inline";
+    }
+  };
